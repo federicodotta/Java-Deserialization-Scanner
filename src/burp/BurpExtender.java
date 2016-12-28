c7b634873a46caca65e3f4110805290481742613
Jeremy Goldstein <jg-dev@live.com>
2016-12-28 22:32:49 +0100

    Update url encoding of base64 gzip for manual tester and exploiter

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1519,7 +1519,7 @@ public class BurpExtender implements IBurpExtender, IScannerCheck, ITab, ActionL
     			} else if(encoding == BurpExtender.TYPE_ASCII_HEX) {
     				request = ArrayUtils.addAll(prePayloadRequest,Hex.encodeHexString(payloadYSoSerial).getBytes());
     			} else if(encoding == BurpExtender.TYPE_BASE64GZIP) {
-                    request = ArrayUtils.addAll(prePayloadRequest,Base64.encodeBase64(gzipData(payloadYSoSerial)));
+                    request = ArrayUtils.addAll(prePayloadRequest,URLEncoder.encode(new String(Base64.encodeBase64(gzipData(payloadYSoSerial)))).getBytes());
                 } else {
                     request = ArrayUtils.addAll(prePayloadRequest,gzipData(payloadYSoSerial));
                 }
@@ -1611,7 +1611,7 @@ public class BurpExtender implements IBurpExtender, IScannerCheck, ITab, ActionL
     			} else if(encoding == BurpExtender.TYPE_ASCII_HEX) {
     				request = ArrayUtils.addAll(prePayloadRequest,Hex.encodeHexString(payloads.get(currentKey)).getBytes());
     			} else if(encoding == BurpExtender.TYPE_BASE64GZIP) {
-                    request = ArrayUtils.addAll(prePayloadRequest,Base64.encodeBase64(gzipData(payloads.get(currentKey))));
+                    request = ArrayUtils.addAll(prePayloadRequest,URLEncoder.encode(new String(Base64.encodeBase64(gzipData(payloads.get(currentKey))))).getBytes());
                 } else {
                     request = ArrayUtils.addAll(prePayloadRequest,gzipData(payloads.get(currentKey)));
                 }
